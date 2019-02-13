ddff8c7cab324aa75118d8410ac56eb5a14d4ec8
jgrenat <jgrenat@users.noreply.github.com>
2019-02-13 21:13:03 +0100

    Add sample imports for RxJS

--- search/search.js
+++ search/search.js
@@ -1,5 +1,7 @@
 import {findCards} from '../pokemon-api.js';
 import {displayCards} from '../utils.js';
+import {fromEvent} from 'https://dev.jspm.io/rxjs@6/_esm2015';
+import {map} from 'https://dev.jspm.io/rxjs@6/_esm2015/operators';
 
 document.getElementById('js-submit').addEventListener('click', () => {
   findCards(document.getElementById('js-search').value, (err, cards) => {
